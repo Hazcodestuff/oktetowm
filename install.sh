5368aef49b09c6ed85047f572eb25b082ead0165
Hazcodestuff <128824758+Hazcodestuff@users.noreply.github.com>
2023-06-22 23:32:30 +0800

    install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+pkg install imagemagick git nodejs ffmpeg libwebp mc nano yarn
+rm -rf session 
+rm -rf node_modules
+yarn
+npm install axios fs
+npm start
+
+echo "All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
